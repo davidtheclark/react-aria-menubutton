868a7d846af0914eccc7133a7a4c2c90f8f36193
David Clark <david.dave.clark@gmail.com>
2015-08-06 18:15:30 -0700

    Update dist

--- dist-modules/Menu.js
+++ dist-modules/Menu.js
@@ -6,7 +6,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'd
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var _react = require('react');
 
@@ -39,6 +39,7 @@ var Menu = (function (_React$Component) {
     var tag = _props.tag;
     var className = _props.className;
     var id = _props.id;
+    var noOverlay = _props.noOverlay;
 
     var childrenToRender = (function () {
       if (typeof children === 'function') {
@@ -48,13 +49,32 @@ var Menu = (function (_React$Component) {
       return [];
     })();
 
-    return _react2['default'].createElement(tag, {
+    var menuEl = _react2['default'].createElement(tag, {
       className: className,
       id: id,
       onKeyDown: manager.handleMenuKey,
       role: 'menu',
-      onBlur: manager.handleBlur
+      onBlur: manager.handleBlur,
+      style: noOverlay ? undefined : { position: 'relative', zIndex: 100 }
     }, childrenToRender);
+
+    if (noOverlay) return menuEl;
+
+    var overlay = !manager.isOpen ? false : _react2['default'].createElement('div', {
+      style: {
+        cursor: 'pointer',
+        position: 'fixed',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        WebkitTapHighlightColor: 'rgba(0,0,0,0)',
+        zIndex: 99
+      },
+      onClick: manager.closeMenu
+    });
+
+    return _react2['default'].createElement('div', {}, menuEl, overlay);
   };
 
   return Menu;
@@ -67,6 +87,7 @@ Menu.propTypes = {
   manager: _react.PropTypes.object.isRequired,
   id: _react.PropTypes.string,
   className: _react.PropTypes.string,
+  noOverlay: _react.PropTypes.bool,
   tag: _react.PropTypes.string
 };
 
